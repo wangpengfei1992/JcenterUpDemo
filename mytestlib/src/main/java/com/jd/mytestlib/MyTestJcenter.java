22e7078db55054414d7f9505d289b18eec44c3b8
wangpengfei <wangpengfei@igetwell.cn>
2017-09-25 11:42:05 +0800

    上传项目

---
+++ mytestlib/src/main/java/com/jd/mytestlib/MyTestJcenter.java
@@ -0,0 +1,12 @@
+package com.jd.mytestlib;/**
+ * Created by jd on 2017/9/25.
+ */
+
+/**
+ * created by wangpengfei at 2017/9/25
+ */
+public class MyTestJcenter {
+    public int add(int a,int b){
+        return a+b;
+    }
+}
